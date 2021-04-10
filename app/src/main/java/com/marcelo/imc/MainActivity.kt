06933406550a7a29e4b347f5f24498965689fd4d
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-10 10:09:20 -0300

    feat:add calcular imc e checar se estao vazias

--- app/src/main/java/com/marcelo/imc/MainActivity.kt
+++ app/src/main/java/com/marcelo/imc/MainActivity.kt
@@ -2,10 +2,53 @@ package com.marcelo.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcelo.imc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val btn_calcular = binding.btnCalcular
+        val mensagem = binding.mensagem
+
+        btn_calcular.setOnClickListener {
+            val editPeso = binding.editPeso.text.toString()
+            val editAltura = binding.editAltura.text.toString()
+
+            if(editPeso.isEmpty()){
+                mensagem.setText("Digite o seu peso")
+            } else if (editAltura.isEmpty()){
+                mensagem.setText("Digite a sua  altura")
+            } else {
+                CalculoDeIMC()
+            }
+        }
+    }
+
+    private fun CalculoDeIMC(){
+        val pesoID = binding.editPeso
+        val alturaID = binding.editAltura
+        val resultado = binding.mensagem
+
+        val peso = Integer.parseInt(pesoID.text.toString())
+        val altura = java.lang.Float.parseFloat(alturaID.text.toString())
+        val imc = peso / (altura * altura)
+
+        val Mensagem = when{
+            imc <= 18.5 -> "Peso Baixo"
+            imc <= 24.9 -> "Peso Normal"
+            imc <= 29.9 -> "Sobrepeso"
+            imc <= 34.9 -> "Obesidade (Grau I)"
+            imc <= 39.9 -> "Obesidade Severa (Grau II)"
+            else -> "Obesidade Mórbida (Grau III)"
+        }
+
+        imc.toString()
+        resultado.setText("IMC: $imc \n $Mensagem")
     }
 }
